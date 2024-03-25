c7f94b76d0cb17c7ba40f5982077f7cebc70cb43
EliteMinion <83222004+EliteMinion@users.noreply.github.com>
2024-03-25 22:14:45 +0200

    Add files via upload

---
+++ WEDSensor.ino
@@ -0,0 +1,62 @@
+#include<Wire.h>
+#include <math.h>
+#include <SD.h>
+#include <SPI.h>
+
+const int MPU=0x68; 
+int16_t X,Y,Z,;
+
+const int chipSelect = 10;
+int transmitPin = 8;
+int measurment = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);  
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  
+  SD.begin(chipSelect);
+ 
+  pinMode(transmitPin, OUTPUT);
+}
+
+void loop() {
+  if(findIntensity()>3.5){
+    digitalWrite(transmitPin,HIGH);
+  }
+
+  printDataToFile(findIntensity());
+
+  delay(100);
+}
+
+float findIntensity(){
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  X=Wire.read()<<8|Wire.read();    
+  Y=Wire.read()<<8|Wire.read();  
+  Z=Wire.read()<<8|Wire.read();
+
+  float pga = sqrt(pow(X, 2) + pow(Y, 2) + pow(Z, 2));
+  float richterIntensity = log10(pga) + 1.5;
+
+  return richterIntensity;
+}
+
+void printDataToFile(float intensity){
+  File dataFile = SD.open("data.txt", FILE_WRITE);
+
+  dataFile.print("measurement ");
+  dataFile.print(measurment);
+  dataFile.print(":");
+  dataFile.println(intensity);
+  dataFile.close();
+
+  measurment++;
+}
