a0cdd70c7ce2b1b27fde341def1facf22c3b7f49
EliteMinion <83222004+EliteMinion@users.noreply.github.com>
2024-04-15 19:36:45 +0300

    Update WEDmain.ino

--- WEDmain.ino
+++ WEDmain.ino
@@ -3,12 +3,11 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+int buzzerGround = 0;
 int diodePin1 = 6;
-int diodePin2 = 7;
 int signalPin = 8;
 int buzzerPin1 = 9;
 int buzzerPin2 = 10;
-int inputDiode = 13;
 
 void setup() {
   Serial.begin(9600);
@@ -17,14 +16,13 @@ void setup() {
 
   pinMode(signalPin, INPUT);
   pinMode(diodePin1, OUTPUT);
-  pinMode(diodePin2, OUTPUT);
   pinMode(buzzerPin1, OUTPUT);
   pinMode(buzzerPin2, OUTPUT);
-  pinMode(inputDiode, INPUT_PULLUP);
+  pinMode(buzzerGround, INPUT_PULLUP);
 }
 
 void loop() {
-  if(digitalRead(signalPin)) {
+  if(digitalRead(signalPin)==1) {
     printDisplay();
     buzzers();
     diodes();
@@ -42,23 +40,18 @@ void printDisplay(){
 }
 
 void buzzers(){
-  analogWrite(buzzerPin1, 64);
-  analogWrite(buzzerPin2, 64);
+  digitalWrite(buzzerPin1, HIGH);
+  digitalWrite(buzzerPin2, HIGH);
 }
 
 void diodes(){
   digitalWrite(diodePin1, HIGH);
-  digitalWrite(diodePin2, LOW);
-  delay(200);
-  digitalWrite(diodePin1, LOW);
-  digitalWrite(diodepin2, HIGH);
-  delay(200);
+  delay(1000);
 }
 
 void reset(){
   lcd.clear();
   digitalWrite(diodePin1, LOW);
-  digitalWrite(diodePin2, LOW);
   digitalWrite(buzzerPin1, LOW);
   digitalWrite(buzzerPin2, LOW);
 }
